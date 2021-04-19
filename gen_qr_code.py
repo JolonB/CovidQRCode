637cee2fe8db82590bcdc821184fba1f23c3cbb9
JolonB <jolon.behrent@gmail.com>
2021-04-19 21:36:30 +1200

    draw text on poster

--- gen_qr_code.py
+++ gen_qr_code.py
@@ -2,7 +2,7 @@ import base64
 import random
 
 import qrcode
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
 values = [
     ("gln", "global location number", "{:013d}".format(random.randrange(10 ** 13))),
@@ -12,6 +12,11 @@ values = [
     ("adr", "address. Use \\n to indicate new lines", None),
 ]
 inputs = {}
+
+TITLE_SIZE = 30
+SUBTITLE_SIZE = 25
+TITLE_FILE = "font/title.ttf"
+SUBTITLE_FILE = "font/subtitle.ttf"
 
 
 def validate_input(
@@ -51,6 +56,33 @@ def draw_on_image(background:Image, foreground:Image, topleft:tuple, bottomright
 
     return background
 
+def remove_n(string:str):
+    return string.replace('\\n', ', ')
+
+def create_poster(foreground:Image, title:str, subtitle:str):
+    # Get poster
+    poster = Image.open('assets/poster_blank.jpg')
+    width, height = poster.size
+
+    # Draw QR code on it
+    topleft = (int(0.2*width), int(0.144*height))
+    bottomright = (int(166/207*width), int(587/1024*height))
+    img = draw_on_image(poster, foreground, topleft, bottomright) # (284,295), (1163,1174) for 1449x2048 poster
+
+    # Add text
+    draw = ImageDraw.Draw(img)
+    title_font = ImageFont.truetype(TITLE_FILE, TITLE_SIZE)
+    subtitle_font = ImageFont.truetype(SUBTITLE_FILE, SUBTITLE_SIZE)
+    title_x0, title_y0, title_x1, title_y1 = title_font.getbbox(title)
+    subtitle_x0, subtitle_y0, subtitle_x1, subtitle_y1 = subtitle_font.getbbox(subtitle)
+
+    title_w = title_x1 - title_x0
+    subtitle_w = subtitle_x1 - subtitle_x0
+    draw.text(((width-title_w)/2, 1215-title_y0), title, font=title_font, fill="black")
+    draw.text(((width-subtitle_w)/2, 1260 - subtitle_y0), subtitle, font=subtitle_font, fill="black") # TODO allow this to be changed based on poster size
+
+    return img
+
 for field, help_text, default in values:
     inputs[field] = validate_input(
         "Enter the {}".format(help_text)
@@ -83,10 +115,17 @@ with open("assets/qrcode_blank") as f:
     # Check if user wants a poster
     poster_yn = validate_input("Would you like this in poster form? If \"n\", only the QR code will be saved. [Y/n]: ", default_value='y', class_=str.lower, valid_values=['y','n'])
     if poster_yn == 'y':
-        poster = Image.open('assets/poster_blank.jpg')
-        img = draw_on_image(poster, img, (284,295), (1163,1174))
+        title = remove_n(inputs['opn'])
+        title = validate_input("Enter a title. Default is {}. \"\\n\" will be replaced by \", \": ".format(title), default_value=title)
+        subtitle = remove_n(inputs['adr'])
+        subtitle = validate_input("Enter a title. Default is {}. \"\\n\" will be replaced by \", \": ".format(subtitle), default_value=subtitle)
+        img = create_poster(img, title, subtitle)        
 
     # Save the image
     filename = "qr_code"
     filename = validate_input("Enter a filename to save as. Default name is {}: ".format(filename), default_value=filename)
     img.save("{}.png".format(filename))
+
+
+# title_font = ImageFont.truetype('font/title.ttf', 20)
+# subtitle_font = ImageFont.truetype('font/subtitle.ttf', 20)
